83916d5764a6d927910461b850328e69e67d8707
bjohnson <brooksjohnson157@gmail.com>
2023-09-19 14:43:01 -0400

    Spring 23

--- force-app/main/default/lwc/refsDemo/refsDemo.js
+++ force-app/main/default/lwc/refsDemo/refsDemo.js
@@ -6,6 +6,10 @@ import {LightningElement} from 'lwc';
 
 export default class RefsDemo extends LightningElement {
 
-    handleSubmit(event){}
+    handleSubmit(){
+       const name = this.refs.name.value
+       const age = this.refs.age.value
+       this.refs.response_ref.innerHTML = `<p>${name} ${age}</p>`
+    }
 
 }
